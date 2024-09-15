5a092d43e0efe61b13657272566c4a36be600866
Sümeyra Polat <180315055@ogr.cbu.edu.tr>
2024-09-15 18:49:31 +0300

    feat: Project setup and initial configurations
    - Created packages following the MVVM architecture pattern.
    - Added mipmap logo design for the app.
    - Integrated Hilt for dependency injection into the project.
    - Granted internet permission in the AndroidManifest.xml.
    - Updated Gradle files with necessary dependencies including Hilt, Room, and Firebase.
    - Added google-services.json file for Firebase configuration.

--- app/src/main/java/com/sumeyra/morethanastory/MainActivity.kt
+++ app/src/main/java/com/sumeyra/morethanastory/MainActivity.kt
@@ -12,7 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sumeyra.morethanastory.ui.theme.MoreThanAStoryTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/sumeyra/morethanastory/di/hiltApp/HiltApplication.kt
@@ -0,0 +1,8 @@
+package com.sumeyra.morethanastory.di.hiltApp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+}
